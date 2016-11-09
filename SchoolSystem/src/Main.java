37fd89efff3a3bcc7413c4ab1e547aae7374fdd8
322954991 <322954991@C-3ZYDYR1.tdsb.on.ca>
2016-11-09 14:45:53 -0500

    finished search program

--- SchoolSystem/src/Main.java
+++ SchoolSystem/src/Main.java
@@ -4,20 +4,25 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner scn=new Scanner(System.in);
-		int[] a=new int [10];
-		for (int i=0;i<a.length;i++)
-		{
-			a[i]=i;
-		}
-		System.out.println("What type of search would you like to use?(Linear/Binary)");
-		String x=scn.nextLine();
-		if(x.equalsIgnoreCase("linear")){
-			System.out.println("What type of variable will you be compareing?(String, double, int)");
-			x=scn.nextLine();
-		}
-			
-		System.out.println(Search.binary(a, scn.nextInt()));
-
+		int[] a= {5,7,19,24,36,42,78};
+		double[] b={3.0,7.8,9.2,9.7,10.8,16.4,20.0};
+		String []c={"A","B","H","K","N","O","P","S","V"};
+		
+		System.out.println(Search.linear(a, 8));
+		System.out.println(Search.linear(b,7.8 ));
+		System.out.println(Search.linear(c, "K"));
+																																										
+		System.out.println(Search.binary(a, 24));
+		System.out.println(Search.binary(a, 36));
+		System.out.println(Search.binary(a, 20));
+		
+		System.out.println(Search.binary(b, 9.2));
+		System.out.println(Search.binary(b, 9.7));
+		System.out.println(Search.binary(b, 16.4));
+		
+		System.out.println(Search.binary(c, "A"));
+		System.out.println(Search.binary(c, "N"));
+		System.out.println(Search.binary(c, "S"));
 	}
-
+																
 }
--- SchoolSystem/src/Search.java
+++ SchoolSystem/src/Search.java
@@ -1,48 +1,136 @@
 
 public class Search {
-	public static int linear(int []a, int target){
-		for (int i=0;i<a.length;i++)
-		{
-			if (a[i]==target)
+	/**
+	 * This method does a linear search for ints
+	 * @param a array of ints
+	 * @param target the element that must be found
+	 * @return int the index of the number in the array(-1 if it isn't in the array)
+	 */
+	public static int linear(int[] a, int target) {
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] == target)
 				return i;
 		}
 		return -1;
 	}
-	public static int linear(double []a, double target){
-		for (int i=0;i<a.length;i++)
-		{
-			if (a[i]==target)
+	/**
+	 * This method does a linear search for doubles
+	 * @param a array of doubles
+	 * @param target the element that must be found
+	 * @return int the index of the number in the array(-1 if it isn't in the array)
+	 */
+	public static int linear(double[] a, double target) {
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] == target)
 				return i;
 		}
 		return -1;
 	}
-	public static int linear(String []a, String target){
-		for (int i=0;i<a.length;i++)
-		{
+	/**
+	 * This method does a linear search for Strings
+	 * @param a array of Strings
+	 * @param target the element that must be found
+	 * @return int the index of the number in the array(-1 if it isn't in the array)
+	 */
+	public static int linear(String[] a, String target) {
+		for (int i = 0; i < a.length; i++) {
 			if (a[i].equals(target))
 				return i;
 		}
 		return -1;
 	}
-	
-	
-	
-	
-	public static int binary(int[]a,int target){
-		return(binary(a,0,a.length-1,target));
+	/**
+	 * This method sets up the binary search method for ints 
+	 * @param a array of ints
+	 * @param target the element that must be found
+	 * @return int the index of the number in the array(-1 if it isn't in the array)
+	 */
+	public static int binary(int[] a, int target) {
+		return (binary(a, 0, a.length - 1, target));
 	}
-	
-	private static int binary(int []a,int start,int end,int target){
-		if (start>end)
+	/**
+	 * This method determines the targets position in the array(int), and returns -1 if it isn't in the array.
+	 * @param a array of ints
+	 * @param start starting index
+	 * @param end ending index
+	 * @param target target the method is searching for
+	 * @return int index of the element in the array
+	 */
+	private static int binary(int[] a, int start, int end, int target) {
+		if (start > end){
 			return -1;
-		int midpoint=(start+end)/2;
-		if (target==a[midpoint])
-			return midpoint;
-		else if (target<a[midpoint])			
-			return binary(a,start,midpoint-1,target);			
-		else if (target>a[midpoint])
-			return binary(a,midpoint+1,end,target);
-		return -1;
+		}
+			int midpoint = (start + end) / 2;
+			if (target == a[midpoint])
+				return midpoint;
+			else if (target < a[midpoint])
+				return binary(a, start, midpoint - 1, target);
+			else if (target > a[midpoint])
+				return binary(a, midpoint + 1, end, target);
+			return -1;
+		
 	}
-	
+	/**
+	 * This method sets up the binary search method for doubles 
+	 * @param a array of doubles
+	 * @param target the element that must be found
+	 * @return int the index of the number in the array(-1 if it isn't in the array)
+	 */
+	public static int binary(double[] a, double target) {
+		return (binary(a, 0, a.length - 1, target));
+	}
+	/**
+	 * This method determines the targets position in the array(double), and returns -1 if it isn't in the array.
+	 * @param a array of doubles
+	 * @param start starting index
+	 * @param end ending index
+	 * @param target target the method is searching for
+	 * @return int index of the element in the array
+	 */
+	private static int binary(double[] a, int start, int end, double target) {
+		if (start > end)
+			return -1;
+		
+			int midpoint = (start + end) / 2;
+			if (target == a[midpoint])
+				return midpoint;
+			else if (target < a[midpoint])
+				return binary(a, start, midpoint - 1, target);
+			else if (target > a[midpoint])
+				return binary(a, midpoint + 1, end, target);
+			return -1;
+		
+	}
+	/**
+	 * This method sets up the binary search method for Strings 
+	 * @param a array of Strings
+	 * @param target the element that must be found
+	 * @return int the index of the number in the array(-1 if it isn't in the array)
+	 */
+	public static int binary(String[] a, String target) {
+		return (binary(a, 0, a.length - 1, target));
+	}
+	/**
+	 * This method determines the targets position in the array(string), and returns -1 if it isn't in the array.
+	 * @param a array of strings
+	 * @param start starting index
+	 * @param end ending index
+	 * @param target target the method is searching for
+	 * @return int index of the element in the array
+	 */
+	private static int binary(String[] a, int start, int end, String target) {
+		if (start > end){
+			return -1;
+		}
+			int midpoint = (start + end) / 2;
+			if (target.compareToIgnoreCase(a[midpoint]) == 0)
+				return midpoint;
+			else if (target.compareToIgnoreCase(a[midpoint]) < 0)
+				return binary(a, start, midpoint - 1, target);
+			else if (target.compareToIgnoreCase(a[midpoint]) > 0)
+				return binary(a, midpoint + 1, end, target);
+			return -1;
+		
+	}
+
 }
