5a9389dfdb6893ebc741ad0329209df6679229f0
322954991 <322954991@C-3ZYDYR1.tdsb.on.ca>
2016-11-21 14:17:47 -0500

    finished bubble sort

--- SchoolSystem/src/Sort.java
+++ SchoolSystem/src/Sort.java
@@ -1,13 +1,13 @@
 /**
- * Jitesh Patel This class sorts arrays in order from least to greatest using either the selection or insertion sorting methods.
+ * Jitesh Patel 
+ * This class sorts arrays in order from least to greatest using either the selection or insertion sorting methods.
  * November 15, 2016
  */
 public class Sort {
 	/**
 	 * This method takes an int array and sorts it using the insertion method
 	 * 
-	 * @param int
-	 *            [] a
+	 * @param int[] a
 	 */
 	public static void insertion(int[] a) {
 		int x = 0;
@@ -28,8 +28,7 @@ public class Sort {
 	/**
 	 * This method takes an string array and sorts it using the insertion method
 	 * 
-	 * @param int
-	 *            [] a
+	 * @param String [] a
 	 */
 	public static void insertion(String[] a) {
 		String x = null;
@@ -50,8 +49,7 @@ public class Sort {
 	/**
 	 * This method takes an double array and sorts it using the insertion method
 	 * 
-	 * @param int
-	 *            [] a
+	 * @param double [] a
 	 */
 	public static void insertion(double[] a) {
 		double x = 0;
@@ -71,8 +69,7 @@ public class Sort {
 	/**
 	 * This method takes an int array and sorts it using the selection method
 	 * 
-	 * @param int
-	 *            [] a
+	 * @param String [] a
 	 */
 	public static void selection(String[] a) {
 		String x = null;
@@ -90,8 +87,7 @@ public class Sort {
 	/**
 	 * This method takes an int array and sorts it using the selection method
 	 * 
-	 * @param int
-	 *            [] a
+	 * @param int [] a
 	 */
 	public static void selection(int[] a) {
 		int x = 0;
@@ -109,8 +105,7 @@ public class Sort {
 	/**
 	 * This method takes an double array and sorts it using the selection method
 	 * 
-	 * @param int
-	 *            [] a
+	 * @param double [] a
 	 */
 	public static void selection(double[] a) {
 		double x = 0;
@@ -124,18 +119,73 @@ public class Sort {
 			}
 		}
 	}
+	/**
+	 * This method takes an int array and sorts it using the bubble method
+	 * 
+	 * @param int [] a
+	 */
 	public static void bubble(int[] a) {
 		int x=0;
-		for (int j=a.length;0<j;j--){
-			for (int i=1;i<a.length;i++){
-				if(a[i]>a[i-1]){
-					x = a[i];
-					a[i] = a[i-1];
-					a[i-1] = x;
-				}
+		int counter=0;
+		for (int i=0;i<a.length;i++){
+			for (int j=0;j<a.length-1;j++){
+				counter=0;
+				if(a[j+1]<a[j]){					
+					x = a[j];
+					a[j] = a[j+1];
+					a[j+1] = x;
+					counter++;
+				}				
 			}
 		}
-		
+		if (counter==0)
+			return;	
+	}
+	/**
+	 * This method takes an double array and sorts it using the bubble method
+	 * 
+	 * @param double [] a
+	 */
+	public static void bubble(double[] a) {
+		double x=0;
+		int counter=0;
+		for (int i=0;i<a.length;i++){
+			for (int j=0;j<a.length-1;j++){
+				counter=0;
+				if(a[j+1]<a[j]){					
+					x = a[j];
+					a[j] = a[j+1];
+					a[j+1] = x;
+					counter++;
+				}
+				
+			}
+		}
+		if (counter==0)
+			return;		
+	}
+	/**
+	 * This method takes an String array and sorts it using the bubble method
+	 * 
+	 * @param string [] a
+	 */
+	public static void bubble(String[] a) {
+		String x=null;
+		int counter=0;
+		for (int i=0;i<a.length;i++){
+			for (int j=0;j<a.length-1;j++){
+				counter=0;
+				if(a[j+1].compareTo(a[j])<0){					
+					x = a[j];
+					a[j] = a[j+1];
+					a[j+1] = x;
+					counter++;
+				}
+				
+			}
+		}
+		if (counter==0)
+			return;		
 	}
 	
 }
